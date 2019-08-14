52fc518d0197204a774acf4096da43ec0fdb2867
Sebastian Mach <sebastian.mach123@gmail.com>
2019-08-14 23:13:57 +0200

    Change thingspeak client to api

--- server/api/routes/measure.js
+++ server/api/routes/measure.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const ThingSpeakClient = require('thingspeakclient')
 const mongoose = require('mongoose')
+const axios = require('axios')
 const client = new ThingSpeakClient()
 const router = express.Router()
 const { thingSpeakWriteKey, thingSpeakReadKey, thingSpeakChannelId } = require('../../config/config')
@@ -13,13 +14,14 @@ router.post('/', async (req, res) => {
     const { temperature, humidity, moisture, lightIntensity, device } = req.body
     const newMeasure = await new Measure({ temperature, humidity, moisture, lightIntensity, device }).save()
     console.log(`Measure saved to mongoDB with id ${newMeasure._id}`)
-
-    client.attachChannel(Number(thingSpeakChannelId), { writeKey: thingSpeakWriteKey, readKey: thingSpeakReadKey }, (errAttachChannel, resAttachChennel) => {
-      if (errAttachChannel) res.status(500).send('Error in thingspeak client')
-      client.updateChannel(Number(thingSpeakChannelId), { field1: temperature, field2: humidity, field3: moisture, field4: lightIntensity }, (updateErr, updateRes) => {
-        res.end('Data received successfully')
-      })
-    })
+    await axios.post(`https://api.thingspeak.com/update?api_key=${thingSpeakWriteKey}&field1=${temperature.value}&field2=${humidity.value}&field3=${moisture.value}&field4=${lightIntensity.value}`)
+    // client.attachChannel(Number(thingSpeakChannelId), { writeKey: thingSpeakWriteKey, readKey: thingSpeakReadKey }, (errAttachChannel, resAttachChennel) => {
+    //   if (errAttachChannel) res.status(500).send('Error in thingspeak client')
+    //   client.updateChannel(Number(thingSpeakChannelId), { field1: temperature, field2: humidity, field3: moisture, field4: lightIntensity }, (updateErr, updateRes) => {
+    //     console.log(updateRes, updateErr)
+    //     res.end('Data received successfully')
+    //   })
+    // })
   } catch (err) {
     res.status(500).send('Error')
   }
