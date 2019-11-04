1007a0f34afddeb984925c8aeb8986770004524e
Sebastian Mach <sebastian.mach123@gmail.com>
2019-11-04 23:19:29 +0100

    Added battery voltage param

--- server/api/routes/measure.js
+++ server/api/routes/measure.js
@@ -12,9 +12,9 @@ const Measure = mongoose.model('measures')
 router.post('/', async (req, res) => {
   try {
     const { temperature, humidity, moisture, lightIntensity, device } = req.body
-    const newMeasure = await new Measure({ temperature, humidity, moisture, lightIntensity, device }).save()
-    console.log(`Measure saved to mongoDB with id ${newMeasure._id}`)
-    await axios.post(`https://api.thingspeak.com/update?api_key=${thingSpeakWriteKey}&field1=${temperature.value}&field2=${humidity.value}&field3=${moisture.value}&field4=${lightIntensity.value}`)
+    // const newMeasure = await new Measure({ temperature, humidity, moisture, lightIntensity, device }).save()
+    // console.log(`Measure saved to mongoDB with id ${newMeasure._id}`)
+    await axios.post(`https://api.thingspeak.com/update?api_key=${thingSpeakWriteKey}&field1=${temperature.value}&field2=${humidity.value}&field3=${moisture.value}&field4=${lightIntensity.value}&field5=${voltage.value}`)
     // client.attachChannel(Number(thingSpeakChannelId), { writeKey: thingSpeakWriteKey, readKey: thingSpeakReadKey }, (errAttachChannel, resAttachChennel) => {
     //   if (errAttachChannel) res.status(500).send('Error in thingspeak client')
     //   client.updateChannel(Number(thingSpeakChannelId), { field1: temperature, field2: humidity, field3: moisture, field4: lightIntensity }, (updateErr, updateRes) => {
