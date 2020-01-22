533c4f1c9df345a6b6c0995e27a97a8ae5c83e3d
Sebastian Mach <sebastian.mach123@gmail.com>
2020-01-22 18:26:45 +0100

    Added thingspeak post

--- server/src/services/measures.js
+++ server/src/services/measures.js
@@ -1,10 +1,16 @@
 const moment = require('moment')
-
+const axios = require('axios')
 const measuresRepository = require('../repositories/measuresRepository')
 
 const saveMeasure = async (measure) => {
   // TODO: handle device not exist
   const savedMeasure = await measuresRepository.saveMeasure(measure)
+  const {temperature, humidity, lightIntensity, soilMoisture, voltage} = measure.data.reduce((acc,cur) => {
+    acc[cur.measure] = cur.value
+    return acc
+  }, {})
+  await axios.post(`https://api.thingspeak.com/update?api_key=9UYST6149KRNPA00&field1=${temperature}&field2=${humidity}&field3=${soilMoisture}&field4=${lightIntensity}&field5=${voltage}`)
+
   return {
     message: 'Measured data successfully saved',
     measureId: savedMeasure._id
